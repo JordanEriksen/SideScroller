1aa8741585a884c54315893db2cc7ca7aca68a4d
lUhyre <eriksenjordan@yahoo.com>
2019-10-22 09:58:07 -0700

    Making the player able to move around and getting the jumping physics and player physics down.

--- Assets/scripts/Playermovement.cs
+++ Assets/scripts/Playermovement.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour{
 
-    public int playerspeed = 10;
+    public int playerspeed = 5;
     public bool facingRight = true;
-    public int playerjumppower = 1250;
+    public int playerjumppower = 100;
     public float moveX;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask WhatIsGround;
+    private int extraJumps;
+    public int extraJumpsValue;
+
+
+    void start(){
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
 
     void Update() {
         playerMove();
+        
+        if(isGrounded == true){
+            extraJumps = 2;
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0){
+            rb.velocity = Vector2.up * playerjumppower;
+            extraJumps--;
+        } else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * playerjumppower;
+        }
     }
     
     void playerMove() {
@@ -25,6 +52,7 @@ public class Playermovement : MonoBehaviour{
 
     void Jump() {
 
+        isGrounded = Physics2D.OverlapCircle(groundcheck.position, checkRadius, WhatIsGround);
     }
 
     void FlipPlayer() {
